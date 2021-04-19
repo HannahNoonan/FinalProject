474bf634a23ab5dd7b800cf2a3fb5eaad41fcdbb
HannahNoonan <hannahnoonan@hotmail.com>
2021-04-19 20:08:18 +0100

    "First commit"

--- main.py
+++ main.py
@@ -1,12 +1,26 @@
 import numpy as np
 import pandas as pd
-import matplotlib as plt
+import matplotlib.pyplot as plt
+import seaborn as sns
 
-dataset = pd.read_csv("amazon bestsellers.csv")
+McDonalds_menu = pd.read_csv("McDonald's Nutritional Info.csv")
 
-print(dataset)
+print(McDonalds_menu.head())
 
-### check null values
-print(dataset.isnull().sum())
+McDonalds_menu.info()
 
-print(dataset.sort_values("Year"))
+calories_per_item = McDonalds_menu[["Category","Item","Calories"]]
+print(calories_per_item)
+McDonalds_menu_ind = calories_per_item.set_index("Item")
+print(McDonalds_menu_ind)
+highest_cals = calories_per_item.sort_values(["Calories","Item","Category"],ascending=[False,True,True]).drop_duplicates("Category")
+print(highest_cals)
+
+sns.barplot(data=highest_cals, y="Calories", x="Item", hue="Category",dodge=False)
+plt.xticks(rotation=90)
+plt.subplots_adjust(bottom=0.5, top=.95)
+plt.title('Highest Calorie Item per Category')
+plt.xlabel('Item')
+plt.ylabel('Calories')
+plt.show()
+
